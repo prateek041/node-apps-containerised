517df9ba0092aff626986704608ec3d4c75cb9cf
prateek041 <prateeksingh9741@gmail.com>
2022-07-24 22:49:00 +0530

    dev and prod compose

--- task-manager-api/index.js
+++ task-manager-api/index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send("hello there nodemon")
+    res.send("hello there nodemon again")
 })
 
 app.listen(port, () => {
